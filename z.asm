a868f7916d03adb601e042223e3ec6e9efbfffaa
Gato <maksymiliancapala1@gmail.com>
2025-03-22 16:59:11 +0100

    Update z.asm

--- z.asm
+++ z.asm
@@ -1,1 +1,18 @@
+section .data
+    hello db 'Hello, World!', 0
 
+section .text
+    global _start
+
+_start:
+    ; Write the string to stdout
+    mov rax, 1          ; syscall number for sys_write
+    mov rdi, 1          ; file descriptor (1 = stdout)
+    mov rsi, hello      ; pointer to the string
+    mov rdx, 13         ; length of the string
+    syscall
+
+    ; Exit the program
+    mov rax, 60         ; syscall number for sys_exit
+    xor rdi, rdi        ; exit code 0
+    syscall
